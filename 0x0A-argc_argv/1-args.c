270b9c652fcdb71a09be6dd8dd50bb6f502f8ab4
chidi samuel <samuelchidi15@gmail.com>
2022-11-08 21:13:01 +0100

    commit 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main (int argc , char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
 printf("the number of arguments passed is = %d\n" , argc);
 return(0);
